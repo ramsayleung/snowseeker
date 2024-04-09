0975c575f3fcae826c7840c0d3c67a7b96e67e84
Ramsay Leung <ramsayleung@gmail.com>
2024-04-08 22:00:36 -0700

    Implement the loading and saving method for Favorites

--- SnowSeeker/Favorites.swift
+++ SnowSeeker/Favorites.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 @Observable
 class Favorites {
+    let savePath = URL.documentsDirectory.appending(path: "savedFavorites")
     private var resorts: Set<String>
     
     private let key = "Favorites"
     
     init() {
         // load our saved data
-        
-        resorts = []
+        do {
+            let data = try Data(contentsOf: savePath)
+            resorts =  try JSONDecoder().decode(Set<String>.self, from: data)
+        } catch {
+            resorts = []
+        }
     }
     
     func contains(_ resort: Resort) -> Bool {
@@ -35,5 +40,11 @@ class Favorites {
     
     func save() {
         // write out our data
+        do {
+            let data = try JSONEncoder().encode(resorts)
+            try data.write(to: savePath)
+        }catch {
+            print("Unable to save data.")
+        }
     }
 }
